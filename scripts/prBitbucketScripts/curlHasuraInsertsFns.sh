01e5f5c6bd108a0b03caa0ca513f1d115143cb0d
igor.efrem <igor.efrem@carasent.com>
2023-11-23 17:32:27 +0100

    fix: Added new debg print

--- scripts/prBitbucketScripts/curlHasuraInsertsFns.sh
+++ scripts/prBitbucketScripts/curlHasuraInsertsFns.sh
@@ -58,7 +58,7 @@ function curlDirectoryDeleteAppVersion(){
 
 function curlDirectoryInsertAndCleanNewAppVersion(){
 
-    LAST_COMMIT_MESSAGE=$(echo -n "${LAST_COMMIT_MESSAGE}" | jq -Rs @json)
+    LAST_COMMIT_MESSAGE=$(echo -n "${LAST_COMMIT_MESSAGE}" | jq -Rs @json | sed 's/^"//' | sed 's/"$//' | sed 's/\\\"//g')
     
     OPERATION_DATA="insert_and_clean_new_app_version (args:{new_service_name:\\\"${serviceName}\\\",new_version:\\\"${VERSION}\\\",new_pr_id:\\\"pr${BITBUCKET_PR_ID}\\\",new_commit_message:\\\"${LAST_COMMIT_MESSAGE}\\\"}){updated_at}"
 
