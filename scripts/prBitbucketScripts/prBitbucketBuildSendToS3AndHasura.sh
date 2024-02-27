a47d7b466bae0398373eb31058c6de80f4a353ef
igor.efrem <igor.efrem@carasent.com>
2024-02-27 19:05:42 +0100

    chore: Fixed typo in scripts

--- scripts/prBitbucketScripts/prBitbucketBuildSendToS3AndHasura.sh
+++ scripts/prBitbucketScripts/prBitbucketBuildSendToS3AndHasura.sh
@@ -4,7 +4,7 @@ echo -e "${BASH_LPURP}create new folder ${BASH_YELLOW}generated${BASH_NC}"
 mkdir -p generated
 
 echo -e "${BASH_LPURP}copy config/env.js to ${BASH_YELLOW}generated ${BASH_LPURP}folder${BASH_NC}"
-cp configs/env.js generated/env.js && && npx tsc configs/env.js --declaration --emitDeclarationOnly --outDir generated --allowJs
+#cp configs/env.js generated/env.js && npx tsc configs/env.js --declaration --emitDeclarationOnly --outDir generated --allowJs
 
 echo -e "${LPURP}install globally pnpm${NC}"
 npm i -g pnpm
