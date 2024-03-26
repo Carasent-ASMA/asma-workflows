ca1f653f98e001c2c46fb8d1e4794059bea11571
igor.efrem <igor.efrem@carasent.com>
2024-03-26 10:40:29 +0100

    chore: Set debug messages

--- scripts/prBitbucketScripts/prBitbucketPrerelease.sh
+++ scripts/prBitbucketScripts/prBitbucketPrerelease.sh
@@ -2,17 +2,18 @@
 set -e
 
 source $(dirname "$0")/variables.sh
-
+echo "variables.sh"
 source $(dirname "$0")/helperFns.sh
-
+echo "helperFns.sh"
 source $(dirname "$0")/curlHasuraInsertsFns.sh
-
+echo "curlHasuraInsertsFns.sh"
 source $(dirname "$0")/prBitbucketFindVersionTypeOrExitIfCommitMsgWrong.sh
-
+echo "prBitbucketFindVersionTypeOrExitIfCommitMsgWrong.sh"
 #source $(dirname "$0")/prBitbucketPersistPrVersion.sh
 
 export VERSION="pr$BITBUCKET_PR_ID"
 
 serviceName=$(jq -r ".name" package.json)
-
+echo "after serviceName var
 source $(dirname "$0")/prBitbucketBuildSendToS3AndHasura.sh
+echo "prBitbucketBuildSendToS3AndHasura.sh
